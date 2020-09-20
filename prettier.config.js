5dceca4811d7159b8024f9f02743734bcac7916c
hyunmo.ahn <hyunmo.ahn@linecorp.com>
2020-09-20 15:53:14 +0900

    Set prettier to automatically match code formatting
    ref. https://prettier.io/

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// https://prettier.io/docs/en/options.html
+
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  arrowParens: 'always',
+};
