ecf1d6a625069e34797094969232d26077075c8a
ginna <ginna9498@gmail.com>
2020-09-20 18:03:43 +0900

    edit webpage component as the layout component changes

--- src/component/home/nav/nav.jsx
+++ src/component/home/nav/nav.jsx
@@ -9,9 +9,9 @@ function Nav() {
                 <strong>OURLIBRARY</strong>
             </div>
             <ul className="mainMenu">
-                <NavLink to="home" activeClassName="navActive"><li>프로젝트 소개</li></NavLink>
-                <NavLink to="info" activeClassName="navActive"><li>팀 소개</li></NavLink>
-                <NavLink to="register" activeClassName="navActive"><li>로그인/회원가입</li></NavLink>
+                <NavLink to="/home" activeClassName="navActive"><li>프로젝트 소개</li></NavLink>
+                <NavLink to="/info" activeClassName="navActive"><li>팀 소개</li></NavLink>
+                <NavLink to="/register" activeClassName="navActive"><li>로그인/회원가입</li></NavLink>
             </ul>
         </nav>
     )
