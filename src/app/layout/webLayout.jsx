34661318ea7c303cf467361735ead5b82446842a
ginna <ginna9498@gmail.com>
2020-09-21 23:33:41 +0900

    arrange code (remove console, fixed import, fixed input scss etc)

--- src/app/layout/webLayout.jsx
+++ src/app/layout/webLayout.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Nav from "../../component/home/nav/nav";
+import Nav from "../../component/nav/nav";
 
 function WebLayout({children}) {
   return (
--- src/component/bookList/booList.jsx
+++ src/component/bookList/booList.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import ServiceLayout from "../../app/layout/serviceLayout";
 
 function BookList() {
