0c5c9545cdf6454d33971fb34bf7af07d791f945
ginna <ginna9498@gmail.com>
2020-09-21 23:17:01 +0900

    arrangement router and create Proceted page Router

--- src/routes/rootRoute.jsx
+++ src/routes/rootRoute.jsx
@@ -6,8 +6,13 @@ import ForgetPassword from '../component/register/forgetPassword/forgetPassword'
 import NotFound from '../component/notFound/notFound';
 import BookList from '../component/bookList/booList';
 import Register from "../component/register/register";
-import Main from "../component/main/main";
 import RegisterEntry from "../component/registerEntry/registerEntry";
+import MyRent from "../component/myRent/myRent";
+import Setting from "../component/setting/setting";
+import MakeGroup from "../component/registerEntry/makeGroup/makeGroup";
+import {getToken} from "../utils/handleToken/handleToken";
+import {useEffect} from "react";
+import {useState} from "react";
 
 
 function RootRoute() {
@@ -18,12 +23,46 @@ function RootRoute() {
       <Route path="/info" component={Info}/>
       <Route path="/register" component={Register}/>
       <Route path='/forgetPassword' component={ForgetPassword}/>
-      <Route path='/bookList' component={BookList}/>
-      <Route path='/service' component={Main}/>
-      <Route path='/registerEntry' component={RegisterEntry}/>
-      <Route component={NotFound}/>
+      <ProtectedRoute path='/registerEntry'>
+        <RegisterEntry />
+      </ProtectedRoute>
+      <ProtectedRoute path='/makeGroup'>
+        <MakeGroup />
+      </ProtectedRoute>
+      <ProtectedRoute path='/service/bookList'>
+        <BookList />
+      </ProtectedRoute>
+      <ProtectedRoute path='/service/setting'>
+        <Setting />
+      </ProtectedRoute>
+      <ProtectedRoute path='/service/myRent'>
+        <MyRent />
+      </ProtectedRoute>
+      <Redirect from='/service' to='/service/bookList'/>
       <Redirect path="*" to="/"/>
+      <Route component={NotFound}/>
     </Switch>
+  )
+}
+
+function ProtectedRoute({children, ...rest}) {
+  // const [isAuthenticated, setIsAuthenticated] = useState(null);
+  const isAuthenticated = getToken();
+
+  // useEffect(() => {
+  //   const token = getToken();
+  //   console.log(token);
+  //   setIsAuthenticated(token);
+  //   return function cleanup() {
+  //     getToken();
+  //   }
+  // },);
+
+  return (
+    <Route
+      {...rest}
+      render={() => isAuthenticated ? children : <Redirect to='/register/signIn'/>}
+    />
   )
 }
 
