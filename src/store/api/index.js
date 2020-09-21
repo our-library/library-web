316584e2c3a28a39b32a9eccffa229d219cb83d6
ginna <ginna9498@gmail.com>
2020-09-21 23:30:19 +0900

    edit Api for setting token in https header

--- src/store/api/index.js
+++ src/store/api/index.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import {getToken} from "../../utils/handleToken/handleToken";
 
 const mockOptions = {
   url: 'string',
@@ -14,14 +15,24 @@ const mockOptions = {
 
 export class Api {
   static async fetch(apiOptions) {
+    const access_token = getToken();
     const axiosConfig = {
       baseURL: "http://api.ourlibrary.co.kr/",
       ...apiOptions,
     };
+
+    if(access_token) {
+      axiosConfig.headers = {
+        'Authorization': `Bearer ${access_token}`
+      }
+    }
+
     const { data } = await axios.request(axiosConfig);
     return data
   }
 }
+
+
 
 
 
