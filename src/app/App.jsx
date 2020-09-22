048ba2401b2318c8566984aa3d468f76374d9ad2
ginna <ginna9498@gmail.com>
2020-09-22 20:21:45 +0900

    hashRouter to BrowserRouter

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import '../style.scss';
-import {HashRouter} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 import RootRoute from "../routes/rootRoute";
 
 
 function App() {
   return (
-    <>
-      <HashRouter>
-        <RootRoute/>
-      </HashRouter>
-    </>
+    <BrowserRouter>
+      <RootRoute/>
+    </BrowserRouter>
   )
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,8 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, '/dist'),
     compress: true,
-    port: 9000
+    port: 9000,
+    historyApiFallback: true
   },
   resolve: {
     alias: {
