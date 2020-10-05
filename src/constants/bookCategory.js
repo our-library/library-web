31ed81d283539bc0c2b1be6599f1b6d29ca4e471
ginna <ginna9498@gmail.com>
2020-10-05 18:14:19 +0900

    add constants and mockup-data

---
+++ src/constants/bookCategory.js
@@ -0,0 +1,224 @@
+export const bookCategory = [
+  {
+    mainCategoryKey: 100,
+    mainCategoryName: '소설',
+    includedCategories: [
+      {
+        subCategoryKey: 101,
+        subCategoryName: '나라별 소설',
+      },
+      {
+        subCategoryKey: 102,
+        subCategoryName: '고전/문학',
+      },
+      {
+        subCategoryKey: 103,
+        subCategoryName: '장르소설',
+      },
+      {
+        subCategoryKey: 104,
+        subCategoryName: '테마소설',
+      },
+    ]
+  },
+  {
+    mainCategoryKey: 200,
+    mainCategoryName: '시/에세이',
+    includedCategories: [
+      {
+        subCategoryKey: 201,
+        subCategoryName: '한국시',
+      },
+      {
+        subCategoryKey: 202,
+        subCategoryName: '외국시',
+      },
+      {
+        subCategoryKey: 203,
+        subCategoryName: '그림/포토 에세이',
+      },
+      {
+        subCategoryKey: 204,
+        subCategoryName: '독서 에세이',
+      },
+      {
+        subCategoryKey: 205,
+        subCategoryName: '명상 에세이',
+      },
+      {
+        subCategoryKey: 206,
+        subCategoryName: '성공 에세이',
+      },
+      {
+        subCategoryKey: 207,
+        subCategoryName: '여행 에세이',
+      },
+      {
+        subCategoryKey: 208,
+        subCategoryName: '연애/사랑 에세이',
+      },
+      {
+        subCategoryKey: 209,
+        subCategoryName: '인물 에세이',
+      },
+      {
+        subCategoryKey: 210,
+        subCategoryName: '삶의 지혜/명언',
+      },
+    ]
+  },
+  {
+    mainCategoryKey: 300,
+    mainCategoryName: '경영/경제',
+    includedCategories: [
+      {
+        subCategoryKey: 301,
+        subCategoryName: '경제',
+      },
+      {
+        subCategoryKey: 302,
+        subCategoryName: '경영',
+      },
+      {
+        subCategoryKey: 303,
+        subCategoryName: '마케팅/세일즈',
+      },
+      {
+        subCategoryKey: 304,
+        subCategoryName: '재테크/투자',
+      },
+      {
+        subCategoryKey: 305,
+        subCategoryName: '창업/취업',
+      },
+    ]
+  },
+  {
+    mainCategoryKey: 400,
+    mainCategoryName: '자기계발',
+    includedCategories: [
+      {
+        subCategoryKey: 401,
+        subCategoryName: '대화/협상',
+      },
+      {
+        subCategoryKey: 402,
+        subCategoryName: '성공/처세',
+      },
+      {
+        subCategoryKey: 403,
+        subCategoryName: '시간관리',
+      },
+      {
+        subCategoryKey: 404,
+        subCategoryName: '인간관계',
+      },
+      {
+        subCategoryKey: 405,
+        subCategoryName: '자기능력계발',
+      },
+    ]
+  },
+  {
+    mainCategoryKey: 500,
+    mainCategoryName: '인문',
+    includedCategories: [
+      {
+        subCategoryKey: 501,
+        subCategoryName: '인문일반',
+      },
+      {
+        subCategoryKey: 502,
+        subCategoryName: '심리',
+      },
+      {
+        subCategoryKey: 503,
+        subCategoryName: '교육학',
+      },
+      {
+        subCategoryKey: 504,
+        subCategoryName: '철학',
+      },
+      {
+        subCategoryKey: 505,
+        subCategoryName: '문학론',
+      },
+      {
+        subCategoryKey: 506,
+        subCategoryName: '언어학/기호학',
+      },
+      {
+        subCategoryKey: 506,
+        subCategoryName: '종교학',
+      },
+      {
+        subCategoryKey: 507,
+        subCategoryName: '신화',
+      },
+    ]
+  },
+  {
+    mainCategoryKey: 600,
+    mainCategoryName: '역사/문화',
+    includedCategories: [
+      {
+        subCategoryKey: 601,
+        subCategoryName: '역사/문화일반',
+      },
+      {
+        subCategoryKey: 602,
+        subCategoryName: '세계사',
+      },
+      {
+        subCategoryKey: 603,
+        subCategoryName: '서양사',
+      },
+      {
+        subCategoryKey: 604,
+        subCategoryName: '동양사',
+      },
+      {
+        subCategoryKey: 605,
+        subCategoryName: '한국사',
+      },
+    ]
+  },
+  {
+    mainCategoryKey: 700,
+    mainCategoryName: '사회',
+    includedCategories: [
+      {
+        subCategoryKey: 601,
+        subCategoryName: '인문일반',
+      },
+      {
+        subCategoryKey: 502,
+        subCategoryName: '심리',
+      },
+      {
+        subCategoryKey: 503,
+        subCategoryName: '교육학',
+      },
+      {
+        subCategoryKey: 504,
+        subCategoryName: '철학',
+      },
+      {
+        subCategoryKey: 505,
+        subCategoryName: '문학론',
+      },
+      {
+        subCategoryKey: 506,
+        subCategoryName: '언어학/기호학',
+      },
+      {
+        subCategoryKey: 506,
+        subCategoryName: '종교학',
+      },
+      {
+        subCategoryKey: 507,
+        subCategoryName: '신화',
+      },
+    ]
+  },
+];
