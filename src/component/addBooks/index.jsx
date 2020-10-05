69fc7d440a5ecdf9f47789aa2938cc85460a541b
ginna <ginna9498@gmail.com>
2020-10-05 18:13:40 +0900

    create add-books component

--- src/component/addBooks/index.jsx
+++ src/component/addBooks/index.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import BookSearchInput from "../bookList/bookSearchInput";
+import SearchResultCard from "./searchResultCard";
 
 function AddBooks() {
   return (
-    <>
-      <h2>AddBooks</h2>
-      <p>content</p>
-    </>
+    <div className="bookListContainer">
+      <div className="bookSearchContainer">
+        <div>
+          <h5 className="space-3x">검색을 통해 책을 등록해 주세요.</h5>
+          <BookSearchInput/>
+        </div>
+      </div>
+      <div className="bookSearchResultContainer">
+        <div className="bookSearchResultSec">
+          <SearchResultCard />
+        </div>
+      </div>
+    </div>
   );
 }
 
