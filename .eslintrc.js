1b3e420d9088aea882d5d00ed9c2454cb7df45ac
ginna <ginna9498@gmail.com>
2020-10-06 23:52:48 +0900

    edit eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,8 @@ module.exports = {
   rules: {
     'import/no-extraneous-dependencies': 0,
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
-    'import/extensions': ['off'],
+    'import/extensions': 'off',
+    "import/no-unresolved": 'off',
     'object-curly-newline': 0,
     'import/prefer-default-export': 0,
     'react/prop-types': 0,
