922e63435c870b504b73a6eb3aac8644278b526f
ginna <ginna9498@gmail.com>
2020-10-14 17:42:57 +0900

    fixed lint testing commit

--- src/component/common/spinner.jsx
+++ src/component/common/spinner.jsx
@@ -2,20 +2,10 @@ import React from 'react';
 
 function Spinner({ className }) {
   return (
-    <svg
-      className={`spinner ${className}`}
-      viewBox="0 0 66 66"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <circle
-        className="path"
-        fill="none"
-        cx="33"
-        cy="33"
-        r="30"
-      />
+    <svg className={`spinner ${className}`} viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+      <circle className="path" fill="none" cx="33" cy="33" r="30" />
     </svg>
-  )
+  );
 }
 
-export default Spinner
+export default Spinner;
