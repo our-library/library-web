2b1819f85d12a908c1229bdd0b9f050ff020a2c5
ginna <ginna9498@gmail.com>
2020-10-17 14:46:49 +0900

    Revert "fixed lint testing commit"
    This reverts commit 922e63435c870b504b73a6eb3aac8644278b526f.

--- src/component/common/spinner.jsx
+++ src/component/common/spinner.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 
 function Spinner({ className }) {
   return (
-    <svg className={`spinner ${className}`} viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
-      <circle className="path" fill="none" cx="33" cy="33" r="30" />
+    <svg
+      className={`spinner ${className}`}
+      viewBox="0 0 66 66"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle
+        className="path"
+        fill="none"
+        cx="33"
+        cy="33"
+        r="30"
+      />
     </svg>
-  );
+  )
 }
 
-export default Spinner;
+export default Spinner
