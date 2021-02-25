6903242c1e2893489669144dbb0f41303aa8088c
ginna <ginna9498@gmail.com>
2021-02-25 15:02:05 +0900

    Separation of webpack config environment

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,10 @@ require('@babel/polyfill');
 module.exports = {
   entry: ['@babel/polyfill', './src/index.jsx'],
   output: {
-    path: path.join(__dirname, '/dist'),
+    path: path.join(__dirname, '/build'),
     publicPath: '/',
     filename: 'bundle.js',
   },
-  mode: 'development',
   module: {
     rules: [
       {
@@ -54,7 +53,7 @@ module.exports = {
     }),
   ],
   devServer: {
-    contentBase: path.join(__dirname, '/dist'),
+    contentBase: path.join(__dirname, '/build'),
     compress: true,
     port: 9000,
     historyApiFallback: true,
