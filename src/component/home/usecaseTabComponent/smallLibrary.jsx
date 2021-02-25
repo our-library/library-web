77c3952b92b791ec7445e23e35bb5c34a66edfc2
ginna <ginna9498@gmail.com>
2021-02-25 15:10:05 +0900

    add usecase-tab-component and scss

---
+++ src/component/home/usecaseTabComponent/smallLibrary.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function HomeUsecaseSmallLibrary() {
+  return (
+    <div className="landingLayout">
+      <div className="subAlertSec">
+        지원 예정입니다. 조금만 기다려 주세요!
+      </div>
+    </div>
+  )
+}
+
+export default HomeUsecaseSmallLibrary
